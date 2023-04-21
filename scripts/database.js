79ab5510dc11b97f1ebf9ddeaa6abc00358ac0e6
Sam Thrasher <samthrasher9@gmail.com>
2023-04-21 13:29:05 -0500

    Adding database

---
+++ scripts/database.js
@@ -0,0 +1,78 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            image: "https://i.natgeofe.com/n/e7bbdc38-6f32-42cc-a59a-fba70c0910db/clownfish_03.jpg?w=636&h=423",
+            species: "clownfish",
+            length: "4.3 Inches",
+            food: "crustaceans",
+            harvestedFrom: "Australia"
+        },
+        {
+            name: "Dory",
+            image: "https://i.natgeofe.com/n/e7629970-7a7c-4980-abaa-e144307bd7cc/blue-tang-nationalgeographic_2175267_4x3.jpg",
+            species: "blue tang",
+            length: "6 inches",
+            food: "algae, small invertebrates",
+            harvestedFrom: "Philippines"
+        },
+        {
+            name: "Goldie",
+            image: "https://media.cnn.com/api/v1/images/stellar/prod/221123105542-fisherman-catches-giant-goldfish-restricted.jpg?c=original",
+            species: "goldfish",
+            length: "4 inches",
+            food: "pellets, flakes, insects",
+            harvestedFrom: "China"
+        },
+
+        {
+            name: "Bubbles",
+            image: "https://a-z-animals.com/media/2022/07/iStock-149081471-1024x793.jpg",
+            species: "pufferfish",
+            length: "5 inches",
+            food: "mollusks, crustaceans",
+            harvestedFrom: "Japan"
+        },
+
+        {
+            name: "Rainbow",
+            image: "https://www.wildtrout.org/imager/general/111/rainbow_trout_4d70ad715b0bdc212e98d22bb9d8106e.jpg",
+            species: "rainbow trout",
+            length: "10 inches",
+            food: "insects, crustaceans",
+            harvestedFrom: "USA"
+        },
+
+        {
+            name: "Sushi",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMFlBBCWx8rIInvGDQxU_X4UE6F7XQDCYSCg&usqp=CAU",
+            species: "tuna",
+            length: "12 inches",
+            food: "small fish, squid",
+            harvestedFrom: "Japan"
+        },
+
+        {
+            name: "Spike",
+            image: "https://upload.wikimedia.org/wikipedia/commons/3/3d/Red_lionfish_near_Gilli_Banta_Island.JPG",
+            species: "lionfish",
+            length: "8 inches",
+            food: "small fish, crustaceans",
+            harvestedFrom: "Red Sea"
+        },
+        {
+            name: "Guppy",
+            image: "https://assets.petco.com/petco/image/upload/f_auto,q_auto/3358374-right-1",
+            species: "endler's guppy",
+            length: "1.5 inches",
+            food: "algae, small invertebrates",
+            harvestedFrom: "Venezuela"
+        }
+    ]
+}
